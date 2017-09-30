441d2c0b27c071a1a32bf2824ff58d0984ac80ec
Paula Perez <Pauliperez_4@hotmail.com>
2017-09-30 16:45:41 -0300

    Archivos Innecesarios

--- admin/inc.head.php
+++ admin/inc.head.php
@@ -7,8 +7,6 @@
 <link rel="stylesheet" href="bower_components/bootstrap/dist/css/bootstrap.min.css">
 <!-- Font Awesome -->
 <link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-<!-- Ionicons -->
-<link rel="stylesheet" href="bower_components/Ionicons/css/ionicons.min.css">
 <!-- Theme style -->
 <link rel="stylesheet" href="dist/css/AdminLTE.min.css">
 <!-- AdminLTE Skins. Choose a skin from the css/skins
